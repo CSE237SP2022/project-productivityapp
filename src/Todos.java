c0f42d829ef6e456782fc2aa1fd6503d3578257d
jaredstier <59907204+jaredstier@users.noreply.github.com>
2022-03-23 14:18:47 -0500

    started filers

--- src/Todos.java
+++ src/Todos.java
@@ -1,5 +1,14 @@
+import java.util.Arrays;
 
 public class Todos {
+	
+	static void alphabeticalSortMethod() {
+		//Arrays.sort()
+	}
+	
+	static void reverseAlphabeticalSortMethod () {
+		//Arrays.sort(null,Collections.reverseOrder());
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
