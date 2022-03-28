c19d704565a1799d2ad980e57145e031a1238fdc
jaredstier <59907204+jaredstier@users.noreply.github.com>
2022-03-28 14:41:31 -0500

    sorting method

--- src/Todos.java
+++ src/Todos.java
@@ -1,18 +1,27 @@
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.*;
 
 public class Todos {
 	
 	static void alphabeticalSortMethod() {
-		//Arrays.sort()
+
+		TreeMap<String, ArrayList<String>> sorted = new TreeMap<>();
+		sorted.putAll(todosMap);
+		for (Map.Entry<String, ArrayList<String>> entry : sorted.entrySet()) {
+			System.out.println(entry.getValue());
+		}
+		
 	}
 	
 	static void reverseAlphabeticalSortMethod () {
 		//Arrays.sort(null,Collections.reverseOrder());
 	}
 
-	public HashMap<String,ArrayList<String>> todosMap;
+	public static HashMap<String,ArrayList<String>> todosMap;
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
